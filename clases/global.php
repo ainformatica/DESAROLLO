90343f6a12e6166a91109cdb0d26585eb8e97d40
ainformatica <99056075+ainformatica@users.noreply.github.com>
2022-02-06 09:19:32 -0600

    Cambio del archivo global

--- clases/global.php
+++ clases/global.php
@@ -1,18 +1,15 @@
 <?php
 //Ip de la pc servidor de base de datos
-define("DB_HOST","51.222.86.251");
+define("DB_HOST","");
 
 //Nombre de la base de datos
-define("DB_NAME", "informat_desarrollo_automatizacion");
+define("DB_NAME", "");
 
 //Usuario de la base de datos
-define("DB_USERNAME", "informat_desarrollo");
+define("DB_USERNAME", "");
 
 //Contraseña del usuario de la base de datos
-define("DB_PASSWORD", "^Kwd{PE^(L&#");
+define("DB_PASSWORD", "");
 
 //definimos la codificación de los caracteres
 define("DB_ENCODE","utf8");
-
-//Definimos una constante como nombre del proyecto
-define("PRO_NOMBRE","ITVentas");
