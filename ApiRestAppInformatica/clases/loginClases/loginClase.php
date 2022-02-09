be4ff771777d85c56b9939ac4abc2b7059defe5b
Josue Evangelista <josue.evangelista@unah.hn>
2022-02-09 03:43:23 -0600

    Se cambio la direccion para encriptar la contrasena

--- ApiRestAppInformatica/clases/loginClases/loginClase.php
+++ ApiRestAppInformatica/clases/loginClases/loginClase.php
@@ -3,7 +3,7 @@
     ob_start(); 
 
     require_once "../../bd/consultas.php";
-    require_once dirname(dirname(__DIR__))."/clases/encriptar_desencriptar.php";
+    require_once "../../../clases/encriptar_desencriptar.php";
     //require_once "../../clases/encriptarPassword/encriptar_desencriptar.php";
 
     //Clase para insertar el token
