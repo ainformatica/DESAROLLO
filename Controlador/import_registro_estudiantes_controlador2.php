05d8fc961dc194aa6b0b3e4d09868807f083e1cc
Aguilar-Alejandra <105871266+Aguilar-Alejandra@users.noreply.github.com>
2023-04-23 17:47:52 -0600

    Correo informatica bloqueado
    Google bloqueó la cuenta porque dice enviar spams, se solicitó apelación para que se habilite nuevamente.

--- Controlador/import_registro_estudiantes_controlador2.php
+++ Controlador/import_registro_estudiantes_controlador2.php
@@ -32,8 +32,8 @@
                          $mail -> isSMTP(); //PARA INDICAR QUE VAMOS A UTILIZAR ESTA TECNOLOGIA
                          $mail -> Host = 'smtp.gmail.com'; //AQUI VAMOS A INDICAR EL HOST
                          $mail -> SMTPAuth = true; //INDICAMOS QUE VAMOS A TENER AUTENTICACION DE ESE MTP
-                         $mail -> Username = 'ainformatica2020@gmail.com'; // AQUI COLOCAREMOS NUESTRO CORREO ELECTRÓNICO
-                         $mail -> Password = 'zbpjaxyvanzaoyod'; //CONTRASEÑA DEL CORREO, no es de acceso sino para que una aplicación se pueda conectar al servidor
+                         $mail -> Username = /*'ainformatica2020@gmail.com'*/'victoria.alejandra.unah@gmail.com'; // AQUI COLOCAREMOS NUESTRO CORREO ELECTRÓNICO
+                         $mail -> Password = /*'zbpjaxyvanzaoyod'*/'flnzbmgkwdzjfzmo'; //CONTRASEÑA DEL CORREO, no es de acceso sino para que una aplicación se pueda conectar al servidor
                          $mail -> SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; //INDICAR LA SEGURIDAD CON LA QUE SE VA A TRABAJAR, LO ENCONTRARAS EN LA CONFIGURACIÓN SMTP DE TU CUENTA, POR EJEMPLO EL CIFRADO 465 SSL Y 587 TLS
                          $mail -> Port = 587;
                  
