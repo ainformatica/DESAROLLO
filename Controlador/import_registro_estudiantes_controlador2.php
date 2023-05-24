faa3557b41e5a554b79bb5bdb7ceba9061509bab
Aguilar-Alejandra <105871266+Aguilar-Alejandra@users.noreply.github.com>
2023-05-24 11:40:23 -0600

    Modificar correo

--- Controlador/import_registro_estudiantes_controlador2.php
+++ Controlador/import_registro_estudiantes_controlador2.php
@@ -32,8 +32,8 @@
                          $mail -> isSMTP(); //PARA INDICAR QUE VAMOS A UTILIZAR ESTA TECNOLOGIA
                          $mail -> Host = /*'smtp.gmail.com'*/'smtp.mail.yahoo.com'; //AQUI VAMOS A INDICAR EL HOST
                          $mail -> SMTPAuth = true; //INDICAMOS QUE VAMOS A TENER AUTENTICACION DE ESE MTP
-                         $mail -> Username = /*'ainformatica2020@gmail.com'*//*'victoria.alejandra.unah@gmail.com'*/'victoria.alejandra_aguilar@yahoo.es'; // AQUI COLOCAREMOS NUESTRO CORREO ELECTRÓNICO
-                         $mail -> Password = /*'zbpjaxyvanzaoyod'*//*'flnzbmgkwdzjfzmo'*/'djvkrsvamrjhoado'; //CONTRASEÑA DEL CORREO, no es de acceso sino para que una aplicación se pueda conectar al servidor
+                         $mail -> Username = /*'ainformatica2020@gmail.com'*/'victoria.alejandra_aguilar@yahoo.es'; // AQUI COLOCAREMOS NUESTRO CORREO ELECTRÓNICO
+                         $mail -> Password = /*'zbpjaxyvanzaoyod'*/'djvkrsvamrjhoado'; //CONTRASEÑA DEL CORREO, no es de acceso sino para que una aplicación se pueda conectar al servidor
                          $mail -> SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; //INDICAR LA SEGURIDAD CON LA QUE SE VA A TRABAJAR, LO ENCONTRARAS EN LA CONFIGURACIÓN SMTP DE TU CUENTA, POR EJEMPLO EL CIFRADO 465 SSL Y 587 TLS
                          $mail -> Port = 587;
                  
